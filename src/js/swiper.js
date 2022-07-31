cae093c811634e5c76eb2fdf171aa02a09ff4f99
lizkizzzz <lizakuvakina0611@mail.ru>
2022-07-31 12:55:19 +0300

    final

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,7 +4,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 let swiper;
 
 
-    swiper = new Swiper('.swiper-container',
+    swiper = new Swiper('.swiper-container-brands',
         {
             modules: [Pagination],
             pagination: {
@@ -12,7 +12,7 @@ let swiper;
                 enabled: true,
                 clickable: true,
                 type: "bullets",
-                hideOnClick: true,
+
             },
             breakpoints: {
                 320: {
@@ -22,19 +22,90 @@ let swiper;
                     init: true,
                     allowSlidePrev: true,
                     allowTouchMove: true,
-
                 },
                 768: {
                     init: false,
                     scrollbar: false,
                     allowSlidePrev: false,
                     allowTouchMove: false,
-                }
+                },
+                1120: {
+                    init: false,
+                    scrollbar: false,
+                    allowSlidePrev: false,
+                    allowTouchMove: false,
+                },
             }
         })
 
+swiper = new Swiper('.swiper-container-types',
+    {
+        modules: [Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+            enabled: true,
+            clickable: true,
+            type: "bullets",
 
-// window.addEventListener('resize', destroySwiper)
+        },
+        breakpoints: {
+            320: {
+                spaceBetween: 16,
+                slidesPerView: 'auto',
+                scrollbar: false,
+                init: true,
+                allowSlidePrev: true,
+                allowTouchMove: true,
+            },
+            768: {
+                init: false,
+                scrollbar: false,
+                allowSlidePrev: false,
+                allowTouchMove: false,
+            },
+            1120: {
+                init: false,
+                scrollbar: false,
+                allowSlidePrev: false,
+                allowTouchMove: false,
+            },
+        }
+    })
+
+swiper = new Swiper('.swiper-container-prices',
+    {
+        modules: [Pagination],
+        pagination: {
+            el: '.swiper-pagination',
+            enabled: true,
+            clickable: true,
+            type: "bullets",
+
+        },
+        breakpoints: {
+            320: {
+                spaceBetween: 16,
+                slidesPerView: 'auto',
+                scrollbar: false,
+                init: true,
+                allowSlidePrev: true,
+                allowTouchMove: true,
+            },
+            768: {
+                init: false,
+                scrollbar: false,
+                allowSlidePrev: false,
+                allowTouchMove: false,
+            },
+            1120: {
+                init: false,
+                scrollbar: false,
+                allowSlidePrev: false,
+                allowTouchMove: false,
+            },
+        }
+    })
+
 
 
 
